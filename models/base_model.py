05d5edbbf94d3c9aa6874b1bf79a0d16451e6219
fabo893 <jose.rosa893@gmail.com>
2021-03-29 21:32:57 +0000

    not finished

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,11 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+
+
+Base = declarative_base()
 
 
 class BaseModel:
@@ -10,10 +15,9 @@ class BaseModel:
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            id = Column(String(60), unique=True, nullable=False, primary_key=True)
+            created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+            updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,7 +35,12 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    """NOT FINISH"""
+    def delete(self):
+
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +50,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        if '_sa_instance_state' in dictionary.keys():
+            dictionary.pop('_sa_instance_state')
+
         return dictionary
