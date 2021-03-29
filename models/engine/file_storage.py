6a36c51b8fde711bd7ce0e9ed3c2eb04aada9d9e
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-29 20:15:29 +0000

    Adds instance method 'delete' and update method 'all'

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage, if a class
+        is specified, it returns of objects of said class"""
+        if cls is None:
+            return FileStorage.__objects
+        dir_same_cls = {}
+        for k, v in FileStorage.__objects.items():
+            if v.__class__ == cls:
+                dir_same_cls[k] = v
+        return dir_same_cls
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +31,15 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes an object from the storage"""
+        if obj is None:
+            return
+
+        key = obj.to_dict()['__class__'] + '.' + obj.id
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
