a5b3d899af35e505f4ce88e526c01fc965cb8153
fabo893 <jose.rosa893@gmail.com>
2021-03-30 17:53:08 +0000

    first step to implement the sqlalchemy

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,20 @@ Base = declarative_base()
 
 
 class BaseModel:
+    """Add classes attributes"""
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            id = Column(String(60), unique=True, nullable=False, primary_key=True)
-            created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-            updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            # storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
--- models/state.py
+++ models/state.py
@@ -2,10 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from models import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state',
+                          cascade='all, delete, delete-orphan')
