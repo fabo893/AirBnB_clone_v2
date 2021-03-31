9d34e8793cff228b50e5b4b41d0373a802a46fba
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-31 12:06:56 +0000

    Package now inmports the corresponding engine library when nedeed and the 'storage' var is now global

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 from os import getenv
 
 
 """
 Depending of the value of the environment variable 'storage' get the value
 """
+storage = None
 if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
