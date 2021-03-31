b7c0e6ca0263416f76b65228eba116226adb1f70
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-31 15:36:50 +0000

    defines the storage and calls reload inside each particular engine type

--- models/__init__.py
+++ models/__init__.py
@@ -6,11 +6,11 @@ from os import getenv
 """
 Depending of the value of the environment variable 'storage' get the value
 """
-storage = None
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-storage.reload()
+    storage.reload()
