98426e7144537dfacd5d714de0810eb9591e3555
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-31 13:44:59 +0000

    Commententing out a relationship sqlalchemy

--- models/city.py
+++ models/city.py
@@ -9,6 +9,6 @@ from models.place import Place
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities", cascade='all, delete, delete-orphan')
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    # places = relationship("Place", backref="cities", cascade='all, delete, delete-orphan')
