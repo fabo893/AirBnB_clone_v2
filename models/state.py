f96e3372d62718f33d68772d7bff53a31f200a69
fabo893 <jose.rosa893@gmail.com>
2021-03-31 04:59:49 +0000

    trying that the program compile

--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models import City
+from models.city import City
 from os import getenv
 
 
