cfeca7c6f978d3309775ea19728da0a523ebb9f4
fabo893 <jose.rosa893@gmail.com>
2021-03-31 22:42:43 +0000

    removing default in latitude and longitude

--- models/place.py
+++ models/place.py
@@ -18,8 +18,8 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True, default=0)
-    longitude = Column(Float, nullable=True, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
