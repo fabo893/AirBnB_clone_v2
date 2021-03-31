4a100cb8b1a05c6418c248be04648b60ac37153a
Gustavo Hornedo <2367@holbertonschool.com>
2021-03-31 21:44:12 +0000

    Removing k,v k:_sa_instance_state from to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -30,12 +30,14 @@ class BaseModel:
                 kwargs["created_at"] = datetime.now()
             else:
                 kwargs['created_at'] = (datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f'))
+                                                          '%Y-%m-%dT%H:%M:%S.'
+                                                          '%f'))
             if "updated_at" not in kwargs:
                 kwargs["updated_at"] = datetime.now()
             else:
                 kwargs['updated_at'] = (datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f'))
+                                                          '%Y-%m-%dT%H:%M:%S.'
+                                                          '%f'))
             if "__class__" in kwargs:
                 del kwargs["__class__"]
             for k, v in kwargs.items():
@@ -48,6 +50,12 @@ class BaseModel:
         if '_sa_instance_state' in cp_dict:
             del cp_dict['_sa_instance_state']
         return '[{}] ({}) {}'.format(cls, self.id, cp_dict)
+
+    def __repr__(self):
+        """
+        Official representation of the object
+        """
+        return self.__str__()
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -67,4 +75,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
