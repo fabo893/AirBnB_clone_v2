5b29a5a06075635a3034177339288562a239e3bc
fabo893 <jose.rosa893@gmail.com>
2021-04-01 01:44:52 +0000

    happy pep8

--- models/amenity.py
+++ models/amenity.py
@@ -15,4 +15,4 @@ class Amenity(BaseModel, Base):
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         place_amenities = relationship("Place", secondary='place_amenity',
-                                   back_populates='amenities')
+                                       back_populates='amenities')
