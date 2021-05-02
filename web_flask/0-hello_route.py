0d7a768caa2d8d81bb9a95949d30217a76d8ac5f
fabo893 <jose.rosa893@gmail.com>
2021-05-02 17:52:32 +0000

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes=False
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -16,5 +16,6 @@ def hello():
     """ Hello method """
     return "Hello HBNB!"
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
