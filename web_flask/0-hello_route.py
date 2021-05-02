a34dc4b1f1e74f981d36819cd839aef7a9d38058
fabo893 <jose.rosa893@gmail.com>
2021-05-02 15:34:19 +0000

    task one

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Hello Flask!
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+
+
+@app.route('/')
+def hello():
+    """ Hello method """
+    return "Hello HBNB!"
+
+if __name__ =="__main__":
+    app.run(debug=True)
