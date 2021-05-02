19a2a735e2428b54d2053fee7e72e40ebc49f37a
fabo893 <jose.rosa893@gmail.com>
2021-05-02 18:15:19 +0000

    adding a second hello method

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Hello Flask!
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ Hello method """
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hello123():
+    """ Second hello method """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
